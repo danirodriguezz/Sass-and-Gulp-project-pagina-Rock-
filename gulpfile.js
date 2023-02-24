4db3d9c64fe7ff8affe8da7d560821495b316226
Daniel Rodriguez <danielrodriguezalonso02@gmail.com>
2023-02-24 10:48:45 +0000

    Video & Festival Content completed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
-const { src, dest, watch} = require("gulp");
+const { src, dest, watch, parallel} = require("gulp");
+//CSS
 const sass = require("gulp-sass")(require("sass"));
 const plumber = require("gulp-plumber");
+// Imágenes
+const webp = require('gulp-webp');
 
 function css (done) {
     src("src/scss/**/*.scss") // Identificar el archivo de SASS
@@ -11,6 +14,17 @@ function css (done) {
     done();// Callback que avisa a gulp cuando llegamos al final
 }
 
+function versionWebp(done) { //Funcion para convetir las imagenes a Webp
+    const opciones = {
+        quality: 50
+    };
+    src("src/img/**/*.{png,jpg}")
+        .pipe(webp(opciones))
+        .pipe(dest("build/img"))
+    
+    done();
+}
+
 function dev(done) {
     watch("src/scss/**/*.scss", css);
 
@@ -18,4 +32,5 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.versionWebp = versionWebp;
+exports.dev = parallel(versionWebp, dev);
